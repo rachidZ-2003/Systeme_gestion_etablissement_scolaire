43d653678683a82312ef9c6dc982268044b89d13
bramslinux <sdgbrams@gmail.com>
2025-09-08 14:11:21 +0000

    first commit

--- school_flutter/lib/utils/auth_utils.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'package:crypto/crypto.dart';
-
-String hashPassword(String password) {
-  return sha256
-      .convert(utf8.encode(password))
-      .toString(); // Exemple simple, préférer bcrypt serveur
-}
