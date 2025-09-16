460c26c4e0bad79a1adc9283d7336f5565a67517
Zonga A Rachid <abdoulrachidzong07@gmail.com>
2025-09-16 16:31:45 +0000

    Ajout des fichiers school_flutter/, suppression de pedagogie/, utilisateurs/, db.sqlite3, et mise à jour de .gitignore

--- utilisateurs/app.py
+++
@@ -1,9 +0,0 @@
-# utilisateurs/apps.py
-#from django.apps import AppConfig
-
-#class UtilisateursConfig(AppConfig):
- #   default_auto_field = 'django.db.models.BigAutoField'
- #   name = 'utilisateurs'
-
-  #  def ready(self):
-   #     import utilisateurs.signals
